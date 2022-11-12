fbf2a9b7b99846a1a6ba602423f6666d3322f32e
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2022-11-11 22:04:32 -0600

    initial Spring Initializr push

---
+++ src/main/java/com/at/ManageItBackend/ManageItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.at.ManageItBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageItBackendApplication.class, args);
+	}
+
+}
